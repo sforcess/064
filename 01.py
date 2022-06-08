410ced36ecfd58c910baef0def09d7a1bfda92f3
ekirpichev <ekirpichev@mfisoft.ru>
2022-06-08 14:32:03 +0300

    Initial commit 01

---
+++ 01.py
@@ -0,0 +1,9 @@
+
+user = input('Натыкай что нито: ')
+print('привет: ' + user + '!')
+
+i=1
+while i < 100:
+    i=i+5
+    print(i)
+print('end')
---
+++ 02.py
@@ -0,0 +1,15 @@
+# a = input('input first number: ')
+# b = input('input second number: ')
+#
+# print(f'{int(a) + int(b)}')
+#
+
+
+def rotate(a, b):
+    """ Описание функции rotate"""
+    print(a + b)
+
+
+# rotate(2, 2)
+print(rotate.__doc__)
+
